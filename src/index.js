3bd68bcf2ca7d167ad345934e29f886f1bb6b2dc
ZRRufino <zrrufino7@gmail.com>
2021-07-11 12:28:09 -0500

    config babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// components
+import HelloWorld from './components/HelloWorld';
+
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app') );
